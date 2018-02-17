2f1369eb5b26fc94dbc9054319e6853fe3700bb7
Shan Virk <shanuvirk@hotmail.com>
2018-02-17 13:15:02 -0500

    update v17

--- nim/tools/chocolateyInstall.ps1
+++ nim/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'nim'
-$version = '0.11.2'
+$version = '0.17.2'
 $fileType = 'exe'
 $installArgs = '/S'
-$url = 'http://nim-lang.org/download/nim-0.11.2_x32.exe'
-$url64 = 'http://nim-lang.org/download/nim-0.11.2_x64.exe'
+$url = 'https://nim-lang.org/download/nim-0.17.2_x32.zip'
+$url64 = 'https://nim-lang.org/download/nim-0.17.2_x64.zip'
 
 Install-ChocolateyPackage $packageName $fileType $installArgs $url $url64
