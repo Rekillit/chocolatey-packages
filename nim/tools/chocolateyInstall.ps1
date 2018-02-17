e992827c0cf6f4a4d1ffe83a99efad14a7327786
Shan Virk <shanuvirk@hotmail.com>
2018-02-17 13:45:16 -0500

    update for zip install

--- nim/tools/chocolateyInstall.ps1
+++ nim/tools/chocolateyInstall.ps1
@@ -1,8 +1,15 @@
 ﻿$packageName = 'nim'
 $version = '0.17.2'
-$fileType = 'exe'
-$installArgs = '/S'
+$fileType = 'zip'
 $url = 'https://nim-lang.org/download/nim-0.17.2_x32.zip'
+$unzip = '$env:ChocolateyBinRoot'
 $url64 = 'https://nim-lang.org/download/nim-0.17.2_x64.zip'
 
-Install-ChocolateyPackage $packageName $fileType $installArgs $url $url64
+Install-ChocolateyZipPackage -PackageName $packageName $url $unzip $url64
+
+$path = '$env:ChocolateyBinRoot/nim-0.17.2/bin'
+
+Install-ChocolateyPath `
+  -PathToInstall $path `
+  -PathType 'Machine'
+
