b0548286c524e0ad9227e23eaf096e377802b0d1
Shan Virk <shanuvirk@hotmail.com>
2018-02-19 02:11:05 -0500

    update binRoot

--- nim/tools/chocolateyInstall.ps1
+++ nim/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $version = '0.17.2'
 $url = 'https://nim-lang.org/download/nim-0.17.2_x32.zip'
 $url64 = 'https://nim-lang.org/download/nim-0.17.2_x64.zip'
 
-$binRoot = Get-BinRoot
+$binRoot = Get-ToolsLocation
 Write-Debug "Bin Root is $binRoot"
 
 Install-ChocolateyZipPackage "$packageName" $url $binRoot $url64
